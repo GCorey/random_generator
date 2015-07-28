0697d2b33524f78547b883f8ca1aa6b716c09782
gcorey <mr.gcoreyhagan@gmail.com>
2015-07-28 18:39:59 -0400

    Changed the wording on the else statement

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ var guess = prompt("I am thinking of a number between 1 and 6. What is it?");
 if (parseInt(guess) === randomNumber ) {
   alert("You guessed the number!");
 } else {
-  alert("Sorry man, the number was " + randomNumber);
+  alert("You are dead wrong man. " + randomNumber);
   
 } 
